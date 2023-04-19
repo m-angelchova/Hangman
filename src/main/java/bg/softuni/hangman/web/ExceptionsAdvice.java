f2ff213db5be1bc8c641a42f8af1777207f00fa2
Mariela <angelchova@gmail.com>
2023-04-19 14:19:49 +0300

    error handling changes

--- src/main/java/bg/softuni/hangman/web/ExceptionsAdvice.java
+++ src/main/java/bg/softuni/hangman/web/ExceptionsAdvice.java
@@ -2,6 +2,7 @@ package bg.softuni.hangman.web;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -10,17 +11,19 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import java.util.NoSuchElementException;
 
 @ControllerAdvice
-public class NoSuchElementAdvice {
+public class ExceptionsAdvice {
 
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
     @ExceptionHandler(NoSuchElementException.class)
-    public String onNoElementFound() {
+    public String onNoElementFound(Model model) {
+        model.addAttribute("error", "Няма елемент със зададения параметър.");
         return "error";
     }
 
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
     @ExceptionHandler(UsernameNotFoundException.class)
-    public String onNoUsernameFound() {
+    public String onNoUsernameFound(Model model) {
+        model.addAttribute("error", "Няма потребител с този имейл.");
         return "error";
     }
 
